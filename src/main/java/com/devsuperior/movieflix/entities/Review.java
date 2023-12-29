8e137ad822b72971fbe92b566b96b8ea435db276
mackristofer <mackristofer@gmail.com>
2023-12-29 13:15:40 -0300

    Desafio bds05 ok

--- src/main/java/com/devsuperior/movieflix/entities/Review.java
+++ src/main/java/com/devsuperior/movieflix/entities/Review.java
@@ -1,110 +1,82 @@
 package com.devsuperior.movieflix.entities;
 
+import javax.persistence.*;
 import java.io.Serializable;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import java.util.Objects;
 
 @Entity
 @Table(name = "tb_review")
 public class Review implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String text;
-	
-	@ManyToOne
-	@JoinColumn(name = "user_id")
-	private User user;
-	
-	@ManyToOne
-	@JoinColumn(name = "movie_id")
-	private Movie movies;
-	
-	public Review(){
-		
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	
+    @Column(columnDefinition = "TEXT")
+    private String text;
 
-	public Review(Long id, String text, User user, Movie movies) {
-		super();
-		this.id = id;
-		this.text = text;
-		this.user = user;
-		this.movies = movies;
-	}
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
+    public Review(){
 
-	public Long getId() {
-		return id;
-	}
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public Review(Long id, String text, Movie movie, User user) {
+        this.id = id;
+        this.text = text;
+        this.movie = movie;
+        this.user = user;
+    }
 
-	public String getText() {
-		return text;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setText(String text) {
-		this.text = text;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public User getUser() {
-		return user;
-	}
+    public String getText() {
+        return text;
+    }
 
-	public void setUser(User user) {
-		this.user = user;
-	}
-	
-	
-	public Movie getMovie() {
-		return movies;
-	}
+    public void setText(String text) {
+        this.text = text;
+    }
 
+    public Movie getMovie() {
+        return movie;
+    }
 
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
 
-	public void setMovie(Movie movies) {
-		this.movies = movies;
-	}
+    public User getUser() {
+        return user;
+    }
 
+    public void setUser(User user) {
+        this.user = user;
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Review review = (Review) o;
+        return Objects.equals(id, review.id);
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Review other = (Review) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-	
-	
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
